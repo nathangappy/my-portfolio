a9ab7a94e3000dd70369ce1cb269191d63b6b65f
nathangappy <nate.gappy@gmail.com>
2019-12-17 00:42:22 -0500

    fixing links, mobile, tablet bugs

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let mobileIcon = document.getElementById('mobile-icon');
 let mobileNav = document.getElementById('mobile-nav');
 
 mobileIcon.addEventListener('click', () => {
-  mobileNav.classList.toggle('close');
+  mobileNav.classList.toggle('open');
   console.log('mobile clicked');
 });
 
 function closeSideMenu() {
-  mobileNav.classList.toggle('close');
+  mobileNav.classList.toggle('open');
 }
